1b3674d5390244459af082bcca1d7cd6861e9904
Daniel <daniielroig@gmail.com>
2019-01-17 12:54:09 +0100

    Nova carpeta chat

---
+++ Chat/script.js
@@ -0,0 +1,46 @@
+
+var server = new SillyClient(); //create our class
+server.connect("ecv-esup.s.upf.edu:9000","nueva sala");
+
+//this method is called when the server accepts the connection (no ID yet nor info about the room)
+server.on_connect = function(){
+  console.log("Connected to server! :)");  
+};
+
+//this method is called when the server gives the user his ID (ready to start transmiting)
+server.on_ready = function(id){
+  console.log(id)
+};
+
+
+//this methods receives messages from other users (author_id is an unique identifier per user)
+server.on_message = function( author_id, msg ){
+ 	var element =document.createElement("div");
+ 	element.className="msg rec"
+ 	document.querySelector("#chat").appendChild(element)
+	
+	console.log("Nuevo mensage de"+author_id+": "+msg);
+}
+
+function sendMessage(){
+	var element = document.createElement("div");
+	element.innerHTML =input.value;
+	element.className="msg me"
+
+	//To send information to all the other users connected to the same room
+	server.sendMessage(input.value);
+
+	document.querySelector("#chat").appendChild(element);
+	input.value="";
+
+}
+
+var input = document.querySelector("input");
+var button = document.querySelector("#send");
+
+button.addEventListener("click",sendMessage);
+
+
+var server = new SillyClient(); //create our class
+
+
