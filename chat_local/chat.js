c1c2b67d6751692862c270ad4f7aae6c3ced58c2
XaviAPS <xavier.olivares@airplane.solutions>
2019-01-17 16:21:57 +0100

    kkkk

--- chat_local/chat.js
+++ chat_local/chat.js
@@ -1,12 +1,18 @@
 
 function sendMessage(){
-    var element =document.createElement("div");
-    element.setAttribute("id", "right_box");
-    element.className="container"
-    element.innerHTML =input.value;
-    document.querySelector("#chat").appendChild(element)
 
-    console.log("Nuevo mensage de"+author_id+": "+msg);
+    var element1 = document.getElementById("left_box");
+    var clon = element1.cloneNode(true);
+    var label_son = clon.get;
+    console.log(label_son);
+    var element =document.createElement("div");
+    element.id = "left_box";
+    element.className="test_class";
+    element.innerHTML = input.value;
+    console.log(input.value);
+    document.querySelector("#chat").appendChild(clon)
+
+    console.log("Nuevo mensaje de"+author_id+": "+msg);
 }
 
 var input = document.querySelector("input");
