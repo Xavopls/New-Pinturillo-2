59fd843144b8f0b2bab2b1b6d65d36560117d6c0
xaviaps <xavier.olivares@airplane.solutions>
2019-02-04 13:24:39 +0100

    Added public directory

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-var http = require("http"),
-    fs = require("fs");
-
+var express = require('express');
+var app = express();
 var port = 1337;
 
-var server = http.createServer(function(request, response) {
-    filename = '.' + request.url;
-    fs.exists(filename, function(exists) {
-        if(!exists) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
+app.use(express.static('public'));
 
-        if (fs.statSync(filename).isDirectory()) filename += '/index.html'; //Primera ejecucion, para que lea el HTML
-
-        fs.readFile(filename, "binary", function(err, file) {
-            if(err) {
-                response.writeHead(500, {"Content-Type": "text/plain"});
-                response.write(err + "\n");
-                response.end();
-                return;
-            }
-            response.writeHead(200);
-            response.write(file, "binary");
-            response.end();
-        });
-    });
-})
-
-server.listen(port, function (){
+app.listen(port, function () {
     console.log("Server running at port: ", port);
 });
+
 
 // WEB SOCKET
 
 var WebSocketServer = require('websocket').server;
 wsServer = new WebSocketServer({ // create the server
-    httpServer: server //if we already have our HTTPServer in server variable...
+    httpServer: app //if we already have our HTTPServer in server variable...
 });
 
 wsServer.on('request', function(request) {
