02c4fe2ede988cb0726421fb4b85f5d342d7ef70
XavoPls <xavier.olivares@airplane.solutions>
2019-02-04 19:52:18 +0100

    Express-Ws implemented

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// LISTENER SERVER
+
 var express = require('express');
 var app = express();
-var port = 1337;
+var port = 3000;
 
 app.use(express.static('public'));
 
@@ -8,7 +10,24 @@ app.listen(port, function () {
     console.log("Server running at port: ", port);
 });
 
+// WEB SOCKET SERVICE
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({port: 40510})
 
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        console.log('received: %s', message)
+    })
+    setInterval(
+        () => ws.send('AOOOOOOOOOOOOOOOOOOOOOOOOO DURO'),
+        1000
+    )
+})
+
+
+
+
+/*
 // WEB SOCKET
 
 var WebSocketServer = require('websocket').server;
@@ -29,3 +48,4 @@ wsServer.on('request', function(request) {
         console.log("USER IS GONE");// close user connection
     });
 });
+*/
